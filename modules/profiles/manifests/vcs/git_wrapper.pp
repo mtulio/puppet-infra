a7be2a96c09d621ef39ca153ec4d5fb5b0e89c76
Marco Tulio R Braga <marco.braga@chaordicsystems.com>
2017-01-15 01:49:30 -0200

    [profiles-vcs-git] add git ssh wrapper

--- modules/profiles/manifests/vcs/git_wrapper.pp
+++ modules/profiles/manifests/vcs/git_wrapper.pp
@@ -1,54 +1,45 @@
-# == GIT PULL
-# Pull a repo from GIT.
-# This resource prevents 'overrides option' (remove
-# repository and clone again) on option of module 
-# vcsrepo::ensure::latest
+# == GIT SSH WRAPPER
+# Creates a GIT SSH wrapper.
 #
 # == ['ssh_identity']
 # PUPPET relative path of SSH PRIVATE KEY *FILE*.
 # Ex. 'profiles/ssh_keys/git-id_rsa'
 #
-define profiles::vcs::git_pull (
+define profiles::vcs::git_wrapper (
   $ssh_identity = undef,
   $ssh_port     = '22',
   $keys_dir     = '/etc/git/keys',
-  $key_name     = 'producao.cgi',
-  $sh_git_ssh   = 'profiles/git/ssh_git.sh.erb',
-  $sh_git_run   = 'profiles/git/ssh_git.sh.run.erb',
+  $key_name     = 'mykey',
+  $git_wrapper  = 'profiles/git/git-wrapper.erb',
+  $git_ssh      = 'profiles/git/git-wrapper-ssh.sh.erb',
 ) {
+
   $git_repo_dir = $name
   validate_string($identity, $git_repo_dir)
-  $path_sh_git_ssh  = "${keys_dir}/${key_name}-ssh_git.sh"
-  $path_sh_git_run = "${keys_dir}/${key_name}-ssh_git.sh.run"
-  $identity = "${keys_dir}/${key_name}-id_rsa"
+
+  $path_git_wrapper = "${keys_dir}/${key_name}_git-wrapper.sh"
+  $path_git_ssh     = "${keys_dir}/${key_name}_git-wrapper-ssh.sh"
+  $path_identity    = "${keys_dir}/${key_name}_key"
   
   file { $identity :
-    path   => $identity,
+    path   => $path_identity,
     source => "puppet:///modules/${ssh_identity}",
     mode   => '0600',
     owner  => 'root',
     group  => 'root',
-  } -> file { $path_sh_git_ssh :
-    path    => $path_sh_git_ssh,
-    content => template($sh_git_ssh),
+  } -> file { $path_git_wrapper :
+    path    => $path_git_wrapper,
+    content => template($git_wrapper),
     mode    => '0755',
     owner   => 'root',
     group   => 'root',
-  } -> file { $path_sh_git_run :
-    path    => $path_sh_git_run,
-    content => template($sh_git_run),
+  } -> file { $path_git_ssh :
+    path    => $path_git_ssh,
+    content => template($git_ssh),
     mode    => '0755',
     owner   => 'root',
     group   => 'root',
   }
-  exec {'git_pull':
-    cwd     => $git_repo_dir,
-    path    => $::path,
-    command => "${path_sh_git_run} git pull",
-    onlyif  => "test -d ${git_repo_dir}",
-    require => File[$path_sh_git_run],
-  } 
   
-  $message = "# GIT PULL was done. Take a look at log file [/var/log/git/pull.log]"
-  notice($message)
-
+  notice("The GIT wrapper was created at ${path_git_ssh}. You can send git command as a argument of this wrapper.")
+}
